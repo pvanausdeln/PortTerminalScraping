c9e5ecd2644b1fa4b097bdd34438d51bbce173f4
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-03-08 16:39:34 -0800

    delete EvergreenPort, skeleton of AMPLA

---
+++ APMLA/convertToPost.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import json
+import copy
+import requests
+import datetime
+import glob
+
+class baseInfo:
+    postURL = "https://demo-api.iasdispatchmanager.com:8502/v1/shipmentevents"
+
+    shipmentEventBase = {
+    "associatedAssetSize": None,
+    "associatedUnitId": None,
+    "billOfLadingNumber": None,
+    "bookingNumber": None,
+    "bookingOffice": None,
+    "carrierCode": None,
+    "carrierName": None,
+    "city": None,
+    "consigneeName": None,
+    "containerBookingNumber": None,
+    "country": None,
+    "createdBy": None,
+    "customerOrderReferenceNumber": None,
+    "destinationCity": None,
+    "destinationSPLC": None,
+    "destinationState": None,
+    "eventCode": None,
+    "eventName": None,
+    "eventTime": None,
+    "houseBill": None,
+    "importReferenceNumber": None,
+    "latitude": 0,
+    "location": None,
+    "longitude": 0,
+    "masterBill": None,
+    "notes": None,
+    "onHandNumber": None,
+    "originSPLC": None,
+    "originatorCode": None,
+    "originatorId": 0,
+    "originatorName": None,
+    "postalCode": None,
+    "purchaseOrderReferenceNumber": None,
+    "railBillingNumber": None,
+    "reasonCode": None,
+    "reasonName": None,
+    "receiverCode": None,
+    "receiverName": None,
+    "reportSource": None,
+    "reportedBy": None,
+    "resolvedEventId": 0,
+    "resolvedEventOriginalId": 0,
+    "resolvedEventSource": None,
+    "resolvedEventStatus": None,
+    "sealNumber": None,
+    "shipmentReferenceNumber": None,
+    "signedBy": None,
+    "state": None,
+    "stopType": None,
+    "terminalCode": None,
+    "terminalFunction": None,
+    "unitId": None,
+    "unitSize": 0,
+    "unitState": None,
+    "unitTypeCode": None,
+    "vessel": None,
+    "voyageNumber": None,
+    "workOrderNumber": None
+    }
+
+def APMLAPost(container):
+    return
+
+def main(containerList):
+    for container in containerList:
+        APMLAPost(container)
