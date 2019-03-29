020f28e46d4492ba38ee4409313df72a95632884
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-03-29 10:17:53 -0700

    revert testing code + datetime fix

--- TCT/convertToPost.py
+++ TCT/convertToPost.py
@@ -74,7 +74,7 @@ def TCTStep(event):
     if(event.find("PTTGateOut") != -1):
         return("OA","OUTGATE", datetime.datetime.now().strftime('%m-%d-%Y %H:%M:%S'))
     elif(event.find("Delivered") != -1):
-        return("A","ARRIVED",event.split(" ")[1].strptime("%m/%d/%Y %H:%M %p").strftime('%m-%d-%Y %H:%M:%S'))
+        return("A","ARRIVED", datetime.datetime.strptime(event.splitlines()[1],"%m/%d/%Y %H:%M %p").strftime('%m-%d-%Y %H:%M:%S'))
     elif(event.find("Schedule Appointment") != -1):
         return("RN","Pickup Appointment", datetime.datetime.now().strftime('%m-%d-%Y %H:%M:%S'))
     elif(event.find("In Yard") != -1):
@@ -120,9 +120,10 @@ def TCTPost(step):
 
 
 def main(containerList):
-        fileList = glob.glob(r"C:\\Users\\pvanausdeln\\Dropbox (Blume Global)\\Documents\\UiPath\\PortTerminalScraping\\TCT\\ContainerInformation\\"+containerList+".json", recursive = True) #get all the json steps
+    for container in containerList:
+        fileList = glob.glob(r"C:\\Users\\pvanausdeln\\Dropbox (Blume Global)\\Documents\\UiPath\\PortTerminalScraping\\TCT\\ContainerInformation\\"+container+".json", recursive = True) #get all the json steps
         if (not fileList):
-            return
+            continue
         fileList = [f for f in fileList if containerList in f] #set of steps for this number
         fileList.sort(key=os.path.getmtime) #order steps correctly (by file edit time)
         for step in fileList:
