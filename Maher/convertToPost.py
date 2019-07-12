f43ab34909e39c96a4a17b1768e71f328a16da61
sow-blu <49221631+sow-blu@users.noreply.github.com>
2019-07-12 13:47:32 +0530

    Fixed the index issue

--- Maher/convertToPost.py
+++ Maher/convertToPost.py
@@ -101,13 +101,13 @@ def MaherPost(step):
         #The following will be stored if no out activity occurs and if a trucker event the receiver name is added
         if(data["Event"]=="InVessel Activity"  and data["Event_Time"] != ""):
             postJson["eventName"]= "Vessel Arrival"
-            postJson["eventTime"]= datetime.datetime.strptime(data["InVessel Activity"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
+            postJson["eventTime"]= datetime.datetime.strptime(data["Event_Time"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
             postJson["eventCode"]="VA"
             r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
             print(r)
         elif(data["Event"]=="InTruck Activity"  and data["Event_Time"] != ""):
             postJson["eventName"]= "Vessel Arrival"
-            postJson["eventTime"] = datetime.datetime.strptime(data["InTruck Activity"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
+            postJson["eventTime"] = datetime.datetime.strptime(data["Event_Time"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
             postJson["eventCode"]="VA"
             postJson["receiverName"]= data["ReceiverName"]
             r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
@@ -116,20 +116,20 @@ def MaherPost(step):
         #The above event will be overwritten as out Activities happen the latest and if a trucker event then receiver name is added
         if(data["Event"]=="OutVessel Activity" and data["Event_Time"] != ""):
             postJson["eventName"]= "Vessel Departure"
-            postJson["eventTime"]= datetime.datetime.strptime(data["OutVessel Activity"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
+            postJson["eventTime"]= datetime.datetime.strptime(data["Event_Time"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
             postJson["eventCode"]= "VD"
             r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
             print(r)
         elif(data["Event"]=="OutTruck Activity"  and data["Event_Time"] != ""):
             postJson["eventName"]= "Vessel Departure"
-            postJson["eventTime"]= datetime.datetime.strptime(data["OutTruck Activity"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
+            postJson["eventTime"]= datetime.datetime.strptime(data["Event_Time"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
             postJson["eventCode"]= "VD"
             postJson["receiverName"]= data["ReceiverName"]
             r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
             print(r)
         elif(data["Event"]=="OutRail Activity"  and data["Event_Time"] != ""):
             postJson["eventName"]= "Vessel Departure"
-            postJson["eventTime"]= datetime.datetime.strptime(data["OutTruck Activity"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
+            postJson["eventTime"]= datetime.datetime.strptime(data["Event_Time"], '%Y-%m-%d %H:%M:%S').strftime('%m-%d-%Y %H:%M:%S')
             postJson["eventCode"]= "VD"
             postJson["receiverName"]= data["ReceiverName"]
             r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
